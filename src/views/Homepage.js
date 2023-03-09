0804ef3832f461dc7521d1657e7fa9e4fd317a91
Guaguagugu <batato4ni@gmail.com>
2023-03-10 07:34:21 +0800

    0309_eeeee

--- src/views/Homepage.js
+++ src/views/Homepage.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import Search from "../components/Search";
 import "../styles/all.css";
+import Picture from "../components/Picture";
 
 const Homepage = () => {
   const [input, setInput] = useState("");
@@ -9,6 +10,7 @@ const Homepage = () => {
   const auth = "563492ad6f917000010000018ce14e8891404799a12c0e9b7898cf2c";
   const intialURL = "https://api.pexels.com/v1/curated?page=1&per_page=15";
   const search = async () => {
+    console.log("searchhhhh!");
     const dataFetch = await fetch(intialURL, {
       method: "GET",
       headers: {
@@ -16,7 +18,9 @@ const Homepage = () => {
         Authorization: auth
       }
     });
+    // console.log("aaa", await dataFetch.json());
     let parseData = await dataFetch.json();
+    console.log("eee");
     setData(parseData);
   };
   return (
@@ -26,7 +30,18 @@ const Homepage = () => {
       }}
     >
       <h1>this is homepage</h1>
-      <Search search={search} />
+      <Search
+        search={() => {
+          search();
+        }}
+      />
+      <div className="pictures">
+        {/* 等data收到資料 */}
+        {data &&
+          data.photos.map((d) => {
+            return <Picture key={d.id} data={d} />;
+          })}
+      </div>
     </div>
   );
 };
